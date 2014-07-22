9d14eb2216150e6c7b3bce619159b586fcee946c
André Leite <andre.guimaraes.leite@gmail.com>
2014-07-22 11:10:42 -0300

    Makes load_stress module simpler and more organized.

--- load_stress.py
+++ load_stress.py
@@ -2,29 +2,21 @@ from multiprocessing import Pool
 import multiprocessing
 import psutil
 
-loads = []
-def stress(expression):
-    def load_stress(x):
-        i = expression(x)
-        return psutil.cpu_percent(interval=0.1)
-    return load_stress
-
-def _stress(x):
-    i = x**x
+def _load_stress(x):
+    i = expression(x)
     return psutil.cpu_percent(interval=0.1)
 
 def get_low_high_average(results):
     low = min(x for x in results if x > 0)
     high = sorted(results)[-1]
     average = sum(x for x in results)/len(results)
-    print low, high, average
+    return (low, high, average)
 
-if __name__ == '__main__':
+def main(_expression):
+    global expression
+    expression = eval('lambda x: {0}'.format(_expression))
     no_of_cores = multiprocessing.cpu_count()
     p = Pool(processes=no_of_cores)
-    l = map(stress(lambda x: x**x), range(100))
-    print 'l = ', l
-    get_low_high_average(l)
-    k = p.map(_stress, range(100))
-    print 'k = ', k
-    get_low_high_average(k)
+    load_map = p.map(_load_stress, range(100))
+    return get_low_high_average(load_map)
+
