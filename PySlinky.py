15f7bcde5397125e2063e603ac50733b43b986cc
André Leite <andre.guimaraes.leite@gmail.com>
2014-10-01 15:54:39 -0300

    Fixing return error

--- PySlinky.py
+++ PySlinky.py
@@ -9,8 +9,7 @@ app = Flask(__name__)
 @app.route('/load/')
 @app.route('/load/<string:expression>', methods=['GET'])
 def load_stress(expression='x**x'):
-    main(expression)
-    return ip
+    return str(main(expression))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, threaded=True)
